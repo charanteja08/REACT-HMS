bc80b5095ced3e4414867951bdf66decdc259b71
charanteja08 <charantejanalam@gmail.com>
2025-08-09 20:50:58 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import SkilligoWelcome from './Components/Pages/SkilligoWelcome';
-import LoginPage from './Components/Pages/LoginPage';
-import Role from './Components/Pages/Role';
-import SignupPage from './Components/Pages/SignupPage';
-import Menu from './Components/Pages/Menu';
-import SignupPage1 from './Components/Pages/SignupPage1';
-
-import First from './Components/Pages/First';   // Fixed casing to match folder
-import Second from './Components/Pages/Second';
-import Third from './Components/Pages/Third';
-
-import Menu2 from './Components/Pages/Menu2';
-import Menu3 from './Components/Pages/Menu3';
-import Menu4 from './Components/Pages/Menu4';
-
+import logo from './logo.svg';
 import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<SkilligoWelcome />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/role" element={<Role />} />
-        <Route path="/signuppage" element={<SignupPage />} />
-        <Route path="/signuppage1" element={<SignupPage1 />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/first" element={<First />} />
-        <Route path="/second" element={<Second />} />
-        <Route path="/third" element={<Third />} />
-        <Route path="/menu2" element={<Menu2 />} />
-        <Route path="/menu3" element={<Menu3 />} />
-        <Route path="/menu4" element={<Menu4 />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
